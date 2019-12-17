ae5f451764db51ffd8e74c2a19e5869637081d7c
Kyle <47466333+crokadilekyle@users.noreply.github.com>
2019-12-17 16:09:28 -0500

    added api request to websites component

--- src/pages/Webapps.js
+++ src/pages/Webapps.js
@@ -5,44 +5,48 @@ export default class Webapps extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      isLoaded: false
     };
   }
 
   componentDidMount() {
-    let self = this;
     axios
       .get(
-        "https://twinwebdev.com/wp-json/wp/v2/web_application?_fields=title,content,link"
+        "https://twinwebdev.com/wp-json/wp/v2/web_application?_fields=title,link,content"
       )
-      .then(function(response) {
-        self.setState({
-          data: response.data
-        });
-        // console.log(self.state.data[0].title);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+      .then(res =>
+        this.setState({
+          data: res.data,
+          isLoaded: true
+        })
+      )
+      .catch(err => console.log(err));
   }
 
   render() {
-    return (
-      <div>
-        <h1>Web Applications</h1>
+    if (this.state.isLoaded) {
+      return (
+        <div>
+          <h1>Web Applications</h1>
 
-        {this.state.data.map((item, i) => {
-          return (
-            <div key={i}>
-              <h2>{item.title.rendered}</h2>
-              <p dangerouslySetInnerHTML={{ __html: item.content.rendered }} />
-              <p>
-                <a href={item.link}>See More</a>
-              </p>
-            </div>
-          );
-        })}
-      </div>
-    );
+          {this.state.data.map((item, i) => {
+            return (
+              <div key={i}>
+                <h2>{item.title.rendered}</h2>
+                <p
+                  dangerouslySetInnerHTML={{ __html: item.content.rendered }}
+                />
+                <p>
+                  <a href={item.link}>See More</a>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
+    return <h3>Loading...</h3>;
   }
 }
--- src/pages/Websites.js
+++ src/pages/Websites.js
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-export default function Websites(props) {
-  return <h1>Websites</h1>;
+export default class Websites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      isLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://twinwebdev.com/wp-json/wp/v2/websites?_fields=title,link,content"
+      )
+      .then(res =>
+        this.setState({
+          data: res.data,
+          isLoaded: true
+        })
+      )
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    if (this.state.isLoaded) {
+      return (
+        <div>
+          <h1>Websites</h1>
+
+          {this.state.data.map((item, i) => {
+            return (
+              <div key={i}>
+                <h2>{item.title.rendered}</h2>
+                <p
+                  dangerouslySetInnerHTML={{ __html: item.content.rendered }}
+                />
+                <p>
+                  <a href={item.link}>See More</a>
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
+    return <h3>Loading...</h3>;
+  }
 }
